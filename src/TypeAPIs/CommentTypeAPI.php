21f11fa54c99268a3050422a1c7b18438b8096bb
Leonardo Losoviz <leo@getpop.org>
2020-11-17 21:03:03 +0800

    Initial commit

---
+++ src/TypeAPIs/CommentTypeAPI.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\CommentMutationsWP\TypeAPIs;
+
+use PoPSchema\CommentMutations\TypeAPIs\CommentTypeAPIInterface;
+
+/**
+ * Methods to interact with the Type, to be implemented by the underlying CMS
+ */
+class CommentTypeAPI implements CommentTypeAPIInterface
+{
+}
